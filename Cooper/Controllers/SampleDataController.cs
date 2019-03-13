7a43e528dbbd73ad0ed9c15db56e096a367ac1bd
PotrusKaterina <potrus.katya@gmail.com>
2019-03-13 12:47:29 +0200

    Update files for the right connect with the Angular

--- Cooper/Controllers/SampleDataController.cs
+++ Cooper/Controllers/SampleDataController.cs
@@ -7,38 +7,72 @@ using Microsoft.AspNetCore.Mvc;
 namespace Cooper.Controllers
 {
     [Route("api/[controller]")]
-    public class SampleDataController : Controller
+    [ApiController]
+    public class ValuesController : ControllerBase
     {
-        private static string[] Summaries = new[]
+        // GET api/values
+        [HttpGet]
+        public ActionResult Get()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> WeatherForecasts()
+            return new JsonResult("value1");
+        }
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            });
+            return "value";
+        }
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
         }
 
-        public class WeatherForecast
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
         {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
 
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
         }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        //public class SampleDataController : Controller
+        //{
+        //    private static string[] Summaries = new[]
+        //    {
+        //        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //    };
+
+        //    [HttpGet("[action]")]
+        //    public IEnumerable<WeatherForecast> WeatherForecasts()
+        //    {
+        //        var rng = new Random();
+        //        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //        {
+        //            DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
+        //            TemperatureC = rng.Next(-20, 55),
+        //            Summary = Summaries[rng.Next(Summaries.Length)]
+        //        });
+        //    }
+
+        //    public class WeatherForecast
+        //    {
+        //        public string DateFormatted { get; set; }
+        //        public int TemperatureC { get; set; }
+        //        public string Summary { get; set; }
+
+        //        public int TemperatureF
+        //        {
+        //            get
+        //            {
+        //                return 32 + (int)(TemperatureC / 0.5556);
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
