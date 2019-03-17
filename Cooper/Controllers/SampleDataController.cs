8baabecdfb9522b02483d7709bac4dca54eda9e4
PotrusKaterina <potrus.katya@gmail.com>
2019-03-18 00:09:14 +0200

    Added configuration file with logs and created a test log

--- Cooper/Controllers/SampleDataController.cs
+++ Cooper/Controllers/SampleDataController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NLog;
 
 namespace Cooper.Controllers
 {
@@ -14,6 +15,8 @@ namespace Cooper.Controllers
         [HttpGet]
         public ActionResult Get()
         {
+            Logger logger = LogManager.GetLogger("CooperLoger");           
+            logger.Info("info log message. Coopers logs are working");
             return new JsonResult("value1");
         }
         // GET api/values/5
@@ -40,39 +43,5 @@ namespace Cooper.Controllers
         public void Delete(int id)
         {
         }
-        //public class SampleDataController : Controller
-        //{
-        //    private static string[] Summaries = new[]
-        //    {
-        //        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        //    };
-
-        //    [HttpGet("[action]")]
-        //    public IEnumerable<WeatherForecast> WeatherForecasts()
-        //    {
-        //        var rng = new Random();
-        //        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //        {
-        //            DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-        //            TemperatureC = rng.Next(-20, 55),
-        //            Summary = Summaries[rng.Next(Summaries.Length)]
-        //        });
-        //    }
-
-        //    public class WeatherForecast
-        //    {
-        //        public string DateFormatted { get; set; }
-        //        public int TemperatureC { get; set; }
-        //        public string Summary { get; set; }
-
-        //        public int TemperatureF
-        //        {
-        //            get
-        //            {
-        //                return 32 + (int)(TemperatureC / 0.5556);
-        //            }
-        //        }
-        //    }
-        //}
     }
 }
