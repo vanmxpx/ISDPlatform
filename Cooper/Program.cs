a6e368476c008bd81e13613da7e25e77fffa0244
Volodymyr Cherneta <vche@isd.dp.ua>
2019-03-25 14:29:02 +0200

    Unnecessary connection in Program was disabled for testing

--- Cooper/Program.cs
+++ Cooper/Program.cs
@@ -17,16 +17,16 @@ namespace Cooper
         public static void Main(string[] args)
         {
             Console.WriteLine("Starting.\r\n"); 
-            using (var _db = new OracleConnection("Pooling = false; User Id=SYSTEM;Password=qQ1111qQ;Data Source=localhost:1521 /cooper;"))
-            {
-                Console.WriteLine("Open connection...");
-                _db.Open();
+        //    using (var _db = new OracleConnection("Pooling = false; User Id=SYSTEM;Password=qQ1111qQ;Data Source=localhost:1521 /cooper;"))
+        //    {
+        //        Console.WriteLine("Open connection...");
+        //        _db.Open();
 
-                Console.WriteLine("Connected to:" + _db.ServerVersion);
-                Console.WriteLine(_db.ConnectionString);
-                Console.WriteLine("\r\nDone. Press key for exit");
-                Console.ReadKey();
-            }
+        //        Console.WriteLine("Connected to:" + _db.ServerVersion);
+        //        Console.WriteLine(_db.ConnectionString);
+        //        Console.WriteLine("\r\nDone. Press key for exit");
+        //        Console.ReadKey();
+        //    }
             CreateWebHostBuilder(args).Build().Run();
         }
 
