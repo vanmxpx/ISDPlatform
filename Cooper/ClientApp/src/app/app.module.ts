7c8233c8618a738c1eef5e70f57849e1280acb9a
PotrusKaterina <potrus.katya@gmail.com>
2019-04-07 14:24:39 +0300

    Create new sign up and sign in

--- Cooper/ClientApp/src/app/app.module.ts
+++ Cooper/ClientApp/src/app/app.module.ts
@@ -6,27 +6,44 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 import { HttpClient } from 'selenium-webdriver/http';
 import { FirstServiceService } from './first-service.service';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatButtonModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
 
 const appRoutes : Routes = [
-  {path: '', component: SignInComponent}
+  {path: '', redirectTo : '/signIn', pathMatch: 'full'},
+  {path: 'signIn', component: SignInComponent},
+  {path: 'signUp', component: SignUpComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(appRoutes),
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule, 
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatButtonModule 
   ],
   providers: [FirstServiceService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,
+  SignInComponent,
+SignUpComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
+export class InputOverviewExample {}
