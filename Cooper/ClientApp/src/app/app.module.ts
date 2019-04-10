54436dbc131d93e77e34f772945d010bad8a32c0
Mew_boy33 <john.sena0007@gmail.com>
2019-04-10 12:27:22 +0300

    ClientApp bugs fixes

--- Cooper/ClientApp/src/app/app.module.ts
+++ Cooper/ClientApp/src/app/app.module.ts
@@ -4,9 +4,17 @@ import { Routes, RouterModule } from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
+import { AuthGuard } from './guards/auth-guard';
+
 import { HttpClient } from 'selenium-webdriver/http';
 import { FirstServiceService } from './first-service.service';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -16,16 +24,25 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatTableModule} from '@angular/material/table';
 
 const appRoutes : Routes = [
-  {path: '', redirectTo : '/signIn', pathMatch: 'full'},
+  {path: '', redirectTo : '/signIn', pathMatch: 'full'},//??
   {path: 'signIn', component: SignInComponent},
-  {path: 'signUp', component: SignUpComponent}
+  {path: 'signUp', component: SignUpComponent},
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'counter', component: CounterComponent, canActivate: [AuthGuard] },
+  { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthGuard] },
+  // otherwise redirect to home
+   { path: '**', redirectTo: 'home' }
 ]
 
 @NgModule({
   declarations: [
+    NavMenuComponent,
     AppComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
   ],
   imports: [
     BrowserModule,
