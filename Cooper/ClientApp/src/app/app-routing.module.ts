e4c07e6e75cf4eeb4693095df199df31a1870ccc
Mew_boy33 <john.sena0007@gmail.com>
2019-04-10 20:22:52 +0300

    Routes now are located in router-module.

--- Cooper/ClientApp/src/app/app-routing.module.ts
+++ Cooper/ClientApp/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
+import { AuthGuard } from './guards/auth-guard';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/signIn', pathMatch: 'full' },
+  { path: 'signIn', component: SignInComponent },
+  { path: 'signUp', component: SignUpComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'counter', component: CounterComponent },
+  { path: 'fetch-data', component: FetchDataComponent , canActivate: [AuthGuard] },
+
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports:
+    [
+      RouterModule.forRoot(routes)
+    ],
+  exports:
+    [
+      RouterModule
+    ],
+  declarations: []
 })
 export class AppRoutingModule { }
