2c1f95210c9561ee49c145cd41e0fc063efa14d4
Volodymyr Cherneta <vche@isd.dp.ua>
2019-04-18 10:56:44 +0300

    frontend refactoring

--- Cooper/ClientApp/src/app/app-routing.module.ts
+++ Cooper/ClientApp/src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignInComponent } from './components/auth/sign-in/sign-in.component';
 import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { GameComponent } from './home/game/game.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameComponent } from './components/home/game/game.component';
 import { GamesComponent } from './components/games/games.component';
 import { GameDetailComponent }  from './components/game-detail/game-detail.component';
 
