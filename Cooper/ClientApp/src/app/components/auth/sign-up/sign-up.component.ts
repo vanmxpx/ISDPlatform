a46abb93e08a32ce079a0ebc1202252adb952acf
Mew_boy33 <john.sena0007@gmail.com>
2019-04-18 21:11:09 +0300

    Stabilizing auth-guard work. Added redirecting for authentificated user from sign in/up pages

--- Cooper/ClientApp/src/app/components/auth/sign-up/sign-up.component.ts
+++ Cooper/ClientApp/src/app/components/auth/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { FormControl,NgForm, FormGroupDirective, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-sign-up',
@@ -11,11 +12,17 @@ export class SignUpComponent implements OnInit {
   registerForm: FormGroup;
     loading = false;
     submitted = false;
+  
+  constructor(private router: Router) {
+    this.CheckAuthentification();
+  }
 
-
-  constructor(){
-      
-   }
+  CheckAuthentification(): void {
+    const Token: string = localStorage.getItem('JwtCooper');
+    if (Token) {
+      this.router.navigate(['/home']);
+    }
+  }
 
   ngOnInit() {}
 }
