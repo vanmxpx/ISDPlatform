79f0859b2cdba40b4a0e4de0185771d989a24605
Nikita <alnikborovins@gmail.com>
2019-04-22 20:07:24 +0300

    Some fixes for GameReview and Statistics

--- Cooper/Controllers/GameStatisticsController.cs
+++ Cooper/Controllers/GameStatisticsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Cooper.Models;
+using Cooper.Repository;
+using AutoMapper;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Cooper.Controllers
@@ -11,11 +13,18 @@ namespace Cooper.Controllers
     [Route("api/game/statistics")]
     public class GameStatisticsController : ControllerBase
     {
+        StatisticsRepository statisticsRepository;
+
+        public GameStatisticsController()
+        {
+            statisticsRepository = new StatisticsRepository();
+        }
+
         // GET: api/<controller>
         [HttpGet]
         public IEnumerable<Statistics> GetAllStatistics()
         {
-            return new List<Statistics>();
+            return statisticsRepository.GetAll();
         }
 
         public IEnumerable<Statistics> GetAllStatisticsByUserId(long userId)    // get all game statistics for concrete user
@@ -32,14 +41,33 @@ namespace Cooper.Controllers
 
         public Statistics GetStatisticsById(long id)
         {
-            return new Statistics();
+            return statisticsRepository.Get(id);
         }
         
         // POST api/<controller>
         [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(201)]
         public IActionResult Post([FromBody]Statistics statistics)
         {
-            // DAO MISSED
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (statistics.Id == 0)
+            {
+                long id = statisticsRepository.Create(statistics);
+                statistics.Id = id;
+
+                return Ok(statistics);
+            }
+            else
+            {
+                statisticsRepository.Update(statistics);
+
+                return Ok(statistics);
+            }
             return Ok(statistics);
         }
         
@@ -47,7 +75,7 @@ namespace Cooper.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(long id)
         {
-            // DAO MISSED
+            statisticsRepository.Delete(id);
             return Ok();
         }
     }
