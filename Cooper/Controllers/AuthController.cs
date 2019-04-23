e7f1eec6966d1ddf37238cc4ac59d4c4aa7cefc6
Mew_boy33 <john.sena0007@gmail.com>
2019-04-23 13:08:26 +0300

    #5 Adding JWT-token validation on server side

--- Cooper/Controllers/AuthController.cs
+++ Cooper/Controllers/AuthController.cs
@@ -10,6 +10,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using Cooper.Models;
 using Cooper.Repository;
+using Cooper.Configuration;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -34,25 +35,20 @@ namespace Cooper.Controllers
 
             User user = userRepository.GetByNickname(user_auth.Username);
 
-
             if (user != null && user.Password == user_auth.Password)
             {
-                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
-                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
-                var tokeOptions = new JwtSecurityToken(
-                    issuer: "http://localhost:50613/",
-                    audience: "http://localhost:50613/",
-                    claims: new List<Claim>(),
-                    expires: DateTime.Now.AddMinutes(5),
-                    signingCredentials: signinCredentials
-                );
+                var signinCredentials = new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256);
 
-                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                var jwt = new JwtSecurityToken(
+                        issuer: AuthOptions.ISSUER,
+                        audience: AuthOptions.AUDIENCE,
+                        expires: DateTime.Now.AddMinutes(AuthOptions.LIFETIME),
+                        signingCredentials: signinCredentials);
 
-                user.Token = tokenString;
-
-                return Ok(user);
+                string tokenString = new JwtSecurityTokenHandler().WriteToken(jwt);
+                
+                return Ok(new { Token = tokenString });
             }
             else
             {
