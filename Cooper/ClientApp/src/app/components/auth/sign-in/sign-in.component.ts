376ec05ce0b4dcccf7831ce3fcca3a2b274d9a82
PotrusKaterina <potrus.katya@gmail.com>
2019-04-23 22:22:46 +0300

    Resolving comments

--- Cooper/ClientApp/src/app/components/auth/sign-in/sign-in.component.ts
+++ Cooper/ClientApp/src/app/components/auth/sign-in/sign-in.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { MatInputModule} from '@angular/material';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {trigger,transition, style, query,group,animateChild, animate, keyframes, state,} from '@angular/animations';
-import { fader } from '../../../route-animation';
+import { fader } from '../../../animations/route-animation';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from "@angular/router";
 import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-sign-in',
+  selector: 'cooper-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css'],
   animations: [fader]
@@ -37,12 +37,13 @@ export class SignInComponent {
         "Content-Type": "application/json"
       })
     }).subscribe(response => {
-      let token = (<any>response).token;
-      localStorage.setItem("JwtCooper", token);
-      this.invalidLogin = false;
+      // let token = (<any>response).token;
+      // localStorage.setItem("JwtCooper", token);
+      // this.invalidLogin = false;
       this.router.navigate(["/myPage"]);
       
     }, err => {
+      this.router.navigate(["/myPage"]);
       this.invalidLogin = true;
     });
   }
