fa40ea0a6d95ace5929dd2289afc60008775ffa6
Mew_boy33 <pugach@dlit.dp.ua>
2019-05-08 14:35:27 +0300

    #5 Implemented TokenFactory for creating instance of JWT token

---
+++ Cooper/Services/AuthorizationService/TokenFactory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Cooper.Configuration;
+using Cooper.Controllers.ViewModels;
+
+namespace Cooper.Services.Authorization
+{
+    public class TokenFactory
+    {
+        private readonly Login login;
+        public TokenFactory(Login login)
+        {
+            this.login = login;
+        }
+
+        public string GetTokenString()
+        {
+            var signinCredentials = new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256);
+
+
+            var identity = GetIdentity(login.Username);
+
+            var jwt = new JwtSecurityToken(
+                        issuer: AuthOptions.ISSUER,
+                        audience: AuthOptions.AUDIENCE,
+                        claims: identity.Claims,
+                        expires: DateTime.Now.AddMinutes(AuthOptions.LIFETIME),
+                        signingCredentials: signinCredentials);
+
+            string tokenString = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return tokenString;
+        }
+
+        private ClaimsIdentity GetIdentity(string username)
+        {
+            var claims = new List<Claim>
+                {
+                    new Claim("username", username)
+                };
+
+            ClaimsIdentity claimsIdentity =
+            new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+
+            return claimsIdentity;
+        }
+
+    }
+}
