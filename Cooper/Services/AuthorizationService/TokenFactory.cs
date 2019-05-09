3e8bb3bb42dfa2e6263bd40078df9cdada42b735
Mew_boy33 <pugach@dlit.dp.ua>
2019-05-09 19:05:23 +0300

    #24 Used constants of configProvider service for JWT Token settings

--- Cooper/Services/AuthorizationService/TokenFactory.cs
+++ Cooper/Services/AuthorizationService/TokenFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,23 +14,28 @@ namespace Cooper.Services.Authorization
     public class TokenFactory
     {
         private readonly Login login;
-        public TokenFactory(Login login)
+        private readonly IConfigProvider configProvider;
+
+        public TokenFactory(Login login, IConfigProvider configProvider)
         {
+            this.configProvider = configProvider;
             this.login = login;
         }
 
         public string GetTokenString()
         {
-            var signinCredentials = new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256);
+            var key = GetSymmetricSecurityKey(configProvider.JwtToken.Key);
+
+            var signinCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
 
             var identity = GetIdentity(login.Username);
 
             var jwt = new JwtSecurityToken(
-                        issuer: AuthOptions.ISSUER,
-                        audience: AuthOptions.AUDIENCE,
+                        issuer: configProvider.JwtToken.Issuer,
+                        audience: configProvider.JwtToken.Audience,
                         claims: identity.Claims,
-                        expires: DateTime.Now.AddMinutes(AuthOptions.LIFETIME),
+                        expires: DateTime.Now.AddMinutes(configProvider.JwtToken.Lifetime),
                         signingCredentials: signinCredentials);
 
             string tokenString = new JwtSecurityTokenHandler().WriteToken(jwt);
@@ -49,6 +55,11 @@ namespace Cooper.Services.Authorization
                 ClaimsIdentity.DefaultRoleClaimType);
 
             return claimsIdentity;
+        }
+
+        private SymmetricSecurityKey GetSymmetricSecurityKey(string key)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
         }
 
     }
