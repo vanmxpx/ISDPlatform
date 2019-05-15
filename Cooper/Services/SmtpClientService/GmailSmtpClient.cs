08133c45ad2c29731dbebae63dd05b478eaa2fb1
Vlad Chabanenko <just.programmer2226@gmail.com>
2019-05-15 17:29:36 +0300

    Some refactoring

--- Cooper/Services/SmtpClientService/GmailSmtpClient.cs
+++ Cooper/Services/SmtpClientService/GmailSmtpClient.cs
@@ -7,12 +7,15 @@ namespace Cooper.Services
     class GmailSmtpClient : ISmtpClient
     {
         public string SmtpServerName {get;} = "smtp.gmail.com";
+
+        private string from;
+        private string password;
         
-        public void SendMail(string from, string to, string password, string subject, string body, string code)
+        public void SendMail(string to, string subject, string body, string token)
         {
             MailMessage msg = new MailMessage(from, to);
             msg.Subject = subject;
-            msg.Body = $"{body}\nYour activate code: {code}";
+            msg.Body = $"{body}\nYour activate code: {token}";
 
             SmtpClient smtp = new SmtpClient(SmtpServerName, 587);
             smtp.EnableSsl = true;
