40b1f7eceb5347b74d0bd1bff91b9c219d5b1973
Vlad Chabanenko <just.programmer2226@gmail.com>
2019-06-19 19:34:41 +0300

    Created a project and added a class library to it to simplify work with the console

---
+++ Tools/OracleDBUpdater/OracleDBUpdater/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ConsoleHelper;
+
+namespace OracleDBUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUtility.WriteLine("Cooper is the best!!!", ConsoleColor.White);
+            ConsoleUtility.ReadLine(ConsoleColor.White);
+        }
+    }
+}
