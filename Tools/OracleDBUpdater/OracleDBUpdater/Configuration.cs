00281d0a1ed93cb39edef8f03032614c5692aff6
Vlad Chabanenko <just.programmer2226@gmail.com>
2019-06-22 08:46:56 +0300

    Rewrite the configuration class to improve readability

--- Tools/OracleDBUpdater/OracleDBUpdater/Configuration.cs
+++ Tools/OracleDBUpdater/OracleDBUpdater/Configuration.cs
@@ -1,65 +1,53 @@
-﻿ using OracleDBUpdater.Exceptions;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace OracleDBUpdater
 {
     /// <summary> Class for convenient work with variables that need to be saved. </summary>
     public static class Configuration
     {
+        /// <summary> Dictionary of all configuration variables. </summary>
+        private static Dictionary<string, string> configurationVariables = new Dictionary<string, string>();
+
         /// <summary> Config file name. </summary>
-        public static readonly string fileName = "configure.txt";
+        public static readonly string fileName = "config.txt";
 
-        /// <summary> Return variable value with name variableName. </summary>
-        /// <exception cref="ConfigurationException"> If the variableName has a space. </exception>
-        public static string GetVariable(string variableName)
+        static Configuration()
         {
-            if (variableName.Contains(' ')) throw new ConfigurationException("Variable can not have a space in the name.");
-
-            if (File.Exists(fileName))
+            if(File.Exists(fileName))
             {
-                string[] lines = File.ReadAllLines(fileName);
-
-                foreach (string line in lines)
+                using (var reader = new StreamReader(fileName))
                 {
-                    if (line.StartsWith($"{variableName} "))
-                    {
-                        return line.Substring(line.IndexOf(' ') + 1);
-                    }
+                    configurationVariables = JsonConvert.DeserializeObject<Dictionary<string, string>>(reader.ReadToEnd());
                 }
             }
+        }
 
-            return null;
+        /// <summary> Return variable value with name variableName. </summary>
+        public static string GetVariable(string variableName)
+        {
+            if (!ContainVariable(variableName)) return null;
+            else return configurationVariables[variableName];
         }
 
         /// <summary> Set value for variable with name variableName</summary>
-        /// <exception cref="ConfigurationException"> If the variableName has a space. </exception>
         public static void SetVariable(string variableName, string value)
         {
-            if (variableName.Contains(' ')) throw new ConfigurationException("Variable can not have a space in the name.");
+            configurationVariables[variableName] = value;
 
-            if (!File.Exists(fileName)) File.Create(fileName).Close();
-
-            List<string> lines = File.ReadAllLines(fileName).ToList();
-            bool isModified = false;
-
-            for (int i = 0; i < lines.Count; i++)
+            using (var writer = new StreamWriter(fileName))
             {
-                if (lines[i].StartsWith($"{variableName} "))
-                {
-                    lines[i] = $"{variableName} {value}";
-                    isModified = true;
-                    break;
-                }
+                string json = JsonConvert.SerializeObject(configurationVariables);
+                writer.WriteLine(json);
             }
+        }
 
-            if (isModified) File.WriteAllLines(fileName, lines);
-            else
-            {
-                lines.Add($"{variableName} {value}");
-                File.WriteAllLines(fileName, lines);
-            }
+        /// <summary> Checks whether the configuration variables for the given variable is contained. </summary>
+        /// <returns> Returns true if variables is exists. </returns>
+        public static bool ContainVariable(string variableName)
+        {
+            return configurationVariables.ContainsKey(variableName);
         }
     }
 }
--- Tools/OracleDBUpdater/OracleDBUpdater/Exceptions/ConfigurationException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace OracleDBUpdater.Exceptions
-{
-    class ConfigurationException : Exception
-    {
-        public ConfigurationException(string msg) : base(msg) { }
-    }
-}
