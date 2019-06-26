c140b73679dc5b0b89b0d91d11bd4c5bedb4a1bd
Yaroslav <yaroslav200124@gmail.com>
2019-06-26 12:32:13 +0300

    Reworking TokenCleaner, CRUD

--- Cooper/Services/TokenCleanerService/TokenCleaner.cs
+++ Cooper/Services/TokenCleanerService/TokenCleaner.cs
@@ -18,62 +18,48 @@ namespace Cooper.Services
             "ENDVERIFYDATE"
         };
 
-        const string table = "TOKENS";
+        const string tokens_table = "TOKENS";
         const string users_table = "USERS";
-        bool timerStart = false;
+        private bool timerStart = false;
         private CRUD crud;
         public TokenCleaner(IConfigProvider configProvider) {
             crud = new CRUD(configProvider);
-            TryToStart(GetMinDateEntities().ToList());
+            RemoveOutdated();
         }
 
-        IEnumerable<VerificationDb> GetMinDateEntities() {
+        void RemoveOutdated() {
             VerificationDb verify;
-            var entities = crud.ReadAll("(SELECT MIN(ENDVERIFYDATE) FROM TOKENS)", "ENDVERIFYDATE", attributes, table);
-            foreach (var entity in entities) {
+            string now = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+            //Get all users that don't verify email
+            var unverified = crud.ReadBellow($"TO_TIMESTAMP(\'{now}\', 'DD.MM.YYYY HH24:MI:SS')", "ENDVERIFYDATE", attributes, tokens_table);
+            var allTokens = crud.ReadFieldValues("t.TOKEN", $"{users_table} u INNER JOIN {tokens_table} t ON u.EMAIL = t.TOKEN");
+            foreach (var entity in unverified) {
                 EntityMapping.Map(entity, out verify);
-                yield return verify;
-            }
-        }
-
-        void RemoveByTokens(IEnumerable<string> tokens) {
-            foreach (string token in tokens) {
-                crud.Delete($"'{token}'", table, "TOKEN");
-                crud.Delete("(SELECT u.ID FROM USERS u INNER JOIN TOKENS t ON u.EMAIL = t.TOKEN)", users_table, "ID");
-                timerStart = false;
-            }
-            TryToStart(GetMinDateEntities().ToList());
-        }
-
-        List<VerificationDb> CheckOutdated(List<VerificationDb> entities) {
-            if (entities.Count > 0 && (entities[0].EndVerifyDate - DateTime.Now).TotalMilliseconds < 0) { //Outdated
-                RemoveByTokens(entities.Select(item => item.Token));
-                entities = CheckOutdated(GetMinDateEntities().ToList());
-            }
-            
-            return entities;
-        }
-
-        void TryToStart(List<VerificationDb> entities) {
-            if (!timerStart && entities.Count > 0) {
-                entities = CheckOutdated(entities);
-                if (entities.Count > 0) {
-                    Timer timer = new Timer((int)((entities[0].EndVerifyDate - DateTime.Now).TotalMilliseconds));
-                    timer.Elapsed += async ( sender, e ) => await Task.Run(()=>RemoveByTokens(entities.Select(item => item.Token)));
-                    timer.Start();
-                    
-                    Console.WriteLine((int)((entities[0].EndVerifyDate - DateTime.Now).TotalMilliseconds));
-                    timerStart = true;
+                
+                if (allTokens.Contains(verify.Token)) {
+                    crud.Delete(verify.Token, users_table, "EMAIL");
                 }
-                else
-                {
-                    timerStart = false;
-                }
+                crud.Delete($"'{verify.Token}'", tokens_table, "TOKEN");
             }
+            TryToStart();
+        }
+
+        string GetMinDate() {
+            return crud.ReadFieldValues("MIN(ENDVERIFYDATE)", tokens_table)[0];
         }
 
         public void TryToStart() {
-            TryToStart(GetMinDateEntities().ToList());
+            if (!timerStart) {
+                string date = GetMinDate();
+                if (date != "") { 
+                    Timer timer = new Timer((int)((DateTime.Parse(date) - DateTime.Now).TotalMilliseconds));
+                    timer.Elapsed += ( sender, e ) => RemoveOutdated();
+                    timer.Start();
+                        
+                    Console.WriteLine((int)((DateTime.Parse(date) - DateTime.Now).TotalMilliseconds));
+                    timerStart = true;
+                }
+            }
         }
     }
 }
