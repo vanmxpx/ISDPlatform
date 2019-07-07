abec23aba465c4c67fd9f75610bb2ae9bc718b18
Mew_boy33 <pugach@dlit.dp.ua>
2019-07-07 16:01:10 +0300

    User model added

---
+++ Cooper/ClientApp/src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    id: number;
+    nickname: string;
+    email: string;
+    photoURL: string;
+
+    // IsVerified: boolean;
+    // IsCreator: boolean;
+    // IsBanned: boolean
+
+    platformLanguage: string;
+    platformTheme: string;
+}
