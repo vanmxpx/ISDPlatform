4c679437b56047a174658031bdeb12cace768f1a
Vlad Chabanenko <just.programmer2226@gmail.com>
2019-07-09 09:20:27 +0300

    Add test for ImageRepostory

---
+++ Tools/MediaServer/MediaServer.Tests/ImageRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using MediaServer.Services;
+using NUnit.Framework;
+using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using Utility;
+
+namespace MediaServer.Tests
+{
+    public class ImageRepositoryTest
+    {
+        private IImageRepository _imageRepository;
+
+        [SetUp]
+        public void Init()
+        {
+            _imageRepository = new MockImageRepository();
+        }
+
+        [Test]
+        public async Task AddAndGetImageAsyncTest1()
+        {
+            await _imageRepository.AddImageAsync(ImageHelper.ToImage(Properties.Resources.TestImage));
+            Image image = await _imageRepository.GetImageAsync(ImageHelper.ToImage(Properties.Resources.TestImage).GetHash());
+            Assert.IsTrue(image != null);
+        }
+    }
+}
