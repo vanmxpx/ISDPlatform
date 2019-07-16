832ad1b47a99eda214e850135407fe284acf45a9
Mew_boy33 <pugach@dlit.dp.ua>
2019-07-16 08:52:24 +0300

    #122 Client refactoring: Basic routing added

--- Cooper/ClientApp/src.v2/app/app-routing.module.ts
+++ Cooper/ClientApp/src.v2/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginLayoutComponent} from './layouts';
+import {LoginLayoutComponent, RegistrationLayoutComponent, PlatformLayoutComponent,
+  PageNotFoundLayoutComponent} from './layouts';
 
 const routes: Routes = [
-{path: 'login', component: LoginLayoutComponent}
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginLayoutComponent},
+  {path: 'registration', component: RegistrationLayoutComponent},
+  {path: 'platform', component: PlatformLayoutComponent},
+  {path: '**', component: PageNotFoundLayoutComponent}
 ];
 
 @NgModule({
