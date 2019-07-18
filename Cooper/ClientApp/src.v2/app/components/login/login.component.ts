0a4cf490527984f7ed08849efcec46323346fb9b
Mew_boy33 <pugach@dlit.dp.ua>
2019-07-18 14:48:43 +0300

    #122 Login moving to new client

--- Cooper/ClientApp/src.v2/app/components/login/login.component.ts
+++ Cooper/ClientApp/src.v2/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'coop-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  failedLoginMessage = 'Invalid username or password.';
+
+  @Output() signIn = new EventEmitter<NgForm>();
+  @Output() socialSignIn = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  signInClicked(form: NgForm): void {
+    this.signIn.emit(form);
+  }
+
+  socialSignInClicked(socialNetwork: string): void {
+    this.socialSignIn.emit(socialNetwork);
+  }
 }
--- Cooper/ClientApp/src.v2/app/layouts/login/login.layout.ts
+++ Cooper/ClientApp/src.v2/app/layouts/login/login.layout.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { UserService } from '@services/model-services/user.service';
+import { MatInputModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {trigger, transition, style, query, group, animateChild, animate, keyframes, state} from '@angular/animations';
+// import { fader } from '../../../animations/route-animation';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'coop-login-layout',
@@ -7,7 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) {
+    this.userService.checkAuthentification();
+  }
+
+  signIn(form: NgForm): void {
+    this.userService.login(JSON.stringify(form.value));
+  }
+
+  socialSignIn(platform: string): void {
+    this.userService.socialSignIn(platform);
+  }
 
   ngOnInit() {
   }
