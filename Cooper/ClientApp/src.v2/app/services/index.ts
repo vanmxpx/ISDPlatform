996404b04c66ba79555a8e60bd98442c449c2fd0
Mew_boy33 <pugach@dlit.dp.ua>
2019-07-21 12:13:42 +0300

    #110 [new client] User model and services added

--- Cooper/ClientApp/src.v2/app/services/index.ts
+++ Cooper/ClientApp/src.v2/app/services/index.ts
@@ -1,3 +1,6 @@
 
 export {AuthentificationService} from './authentification/authentification.service';
 
+export {UserService} from './user.service';
+
+export {UsersInteractionService} from './users-interaction.service'
---
+++ Cooper/ClientApp/src.v2/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '@models';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  async getUserByNickname(nickname: string): Promise<User> {
+    const response = await this.httpClient.get<User>('/users/nickname/' + nickname).toPromise();
+    return response;
+  }
+
+  async getUserByToken(): Promise<User> {
+    const response = await this.httpClient.get<User>('/users/token').toPromise();
+    return response;
+  }
+
+}
