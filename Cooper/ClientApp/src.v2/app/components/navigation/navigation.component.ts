77591d7e2324c7214dd8c48e19cb7976ec81db5e
Dmitriy Karpovskiy <dmkarpovskiy@gmail.com>
2019-07-24 09:59:11 +0300

    constants.less was added(#123)

--- Cooper/ClientApp/src.v2/app/components/navigation/navigation.component.ts
+++ Cooper/ClientApp/src.v2/app/components/navigation/navigation.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'coop-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.less']
 })
 export class NavigationComponent implements OnInit {
 
