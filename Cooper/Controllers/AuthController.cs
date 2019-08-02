3005075e1f390b5b0ab4a596c9146698e6c53367
Vlad Chabanenko <just.programmer2226@gmail.com>
2019-08-02 07:38:26 +0300

    Add XML documentation for Swagger and refactor code Auth, ConfirmationEmail and Registration controllers #151

--- Cooper/Controllers/AuthController.cs
+++ Cooper/Controllers/AuthController.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
-using Cooper.Models;
+﻿using Cooper.Configuration;
 using Cooper.Controllers.ViewModels;
 using Cooper.Repository;
-using Cooper.Configuration;
 using Cooper.Services;
 using Cooper.Services.Authorization;
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cooper.Controllers
 {
 
     [Route("api/auth")]
+    [ApiController]
     public class AuthController : ControllerBase
     {
-        readonly UserRepository userRepository;
-        readonly IConfigProvider configProvider;
-        readonly ISocialAuth socialAuth;
+        private readonly UserRepository userRepository;
+        private readonly IConfigProvider configProvider;
+        private readonly ISocialAuth socialAuth;
 
         public AuthController(IJwtHandlerService jwtService, ISocialAuth socialAuth, IConfigProvider configProvider)
         {
@@ -31,10 +25,25 @@ namespace Cooper.Controllers
             this.socialAuth = socialAuth;
         }
 
-        [HttpPost, Route("login")]
+        /// <summary>
+        /// Checks if a user is registered.
+        /// </summary>
+        /// <param name="login">Login information</param>
+        /// <returns>Token if user is registered</returns>
+        /// <response code="200">If user is registered</response>
+        /// <response code="400">If invalid client request</response>  
+        /// <response code="401">If the user is not registered</response>  
+        [HttpPost]
+        [Route("login")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public IActionResult Login([FromBody]Login login)
         {
             IActionResult result = Unauthorized();
+
             if (login == null)
             {
                 result = BadRequest("Invalid client request");
@@ -43,16 +52,19 @@ namespace Cooper.Controllers
             {
                 login.Username = DbTools.SanitizeString(login.Username);
 
-                if (login.Provider != null && this.socialAuth.getCheckAuth(login.Provider, login.Password, login.ID))
+                if (login.Provider != null && socialAuth.getCheckAuth(login.Provider, login.Password, login.ID))
                 {
-                    if ((login.Username = userRepository.GetByEmail(login.Username)?.Nickname) != null) {
+                    if ((login.Username = userRepository.GetByEmail(login.Username)?.Nickname) != null)
+                    {
                         result = Ok(new { Token = new TokenFactory(login, configProvider).GetTokenString() });
                     }
-                    else {
+                    else
+                    {
                         result = BadRequest("Auth");
                     }
                 }
-                else if (login.Provider == null) {
+                else if (login.Provider == null)
+                {
                     login.Password = DbTools.SanitizeString(login.Password);
 
                     bool authValid = userRepository.CheckCredentials(login.Username, login.Password);
