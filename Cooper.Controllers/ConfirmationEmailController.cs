bff03fed01349eaf06140039324e10464aeda8a5
Vlad Chabanenko <just.programmer2226@gmail.com>
2019-08-06 20:19:35 +0300

    Fix confirmation email controller

--- Cooper.Controllers/ConfirmationEmailController.cs
+++ Cooper.Controllers/ConfirmationEmailController.cs
@@ -1,52 +1,54 @@
-﻿using Cooper.Configuration;
-using Cooper.Repository;
-using Cooper.Services;
+﻿using Cooper.Repositories;
+using Cooper.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-[ApiController]
-public class ConfirmationEmailController : ControllerBase
+namespace Cooper.Controllers
 {
-    private readonly UserRepository userRepository;
-
-    public ConfirmationEmailController(IJwtHandlerService jwtHandler, IConfigProvider configProvider)
+    [ApiController]
+    public class ConfirmationEmailController : ControllerBase
     {
-        userRepository = new UserRepository(jwtHandler, configProvider);
-    }
+        private readonly UserRepository userRepository;
 
-    /// <summary>
-    /// Confirms email by token.
-    /// </summary>
-    /// <remarks>
-    /// If the user is confirmed, then a redirect to "/auth".
-    /// If the user is not confirmed, then he is confirmed and a redirect to "/auth".
-    /// </remarks>
-    /// <returns>A redirect to "/auth"</returns>
-    /// <response code="302">Always</response>  
-    [HttpPost]
-    [Route("confirm")]
-    [Produces("application/json")]
-    [ProducesResponseType(StatusCodes.Status302Found)]
-    public IActionResult Confirm()
-    {
-        IActionResult result;
-
-        string token = Request.Query["token"];
-        string email = userRepository.GetVerifyEmail($"\'{token}\'");
-
-        if (email == null)
+        public ConfirmationEmailController(IJwtHandlerService jwtHandler, IConfigProvider configProvider)
         {
-            result = Redirect("/auth");
-        }
-        else
-        {
-            userRepository.ConfirmEmail(token, email);
-            userRepository.DeleteToken($"\'{token}\'");
-
-            result = Redirect("/auth");
-            //TODO: Auth
+            userRepository = new UserRepository(jwtHandler, configProvider);
         }
 
-        return result;
+        /// <summary>
+        /// Confirms email by token.
+        /// </summary>
+        /// <remarks>
+        /// If the user is confirmed, then a redirect to "/auth".
+        /// If the user is not confirmed, then he is confirmed and a redirect to "/auth".
+        /// </remarks>
+        /// <returns>A redirect to "/auth"</returns>
+        /// <response code="302">Always</response>  
+        [HttpPost]
+        [Route("confirm")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status302Found)]
+        public IActionResult Confirm()
+        {
+            IActionResult result;
+
+            string token = Request.Query["token"];
+            string email = userRepository.GetVerifyEmail($"\'{token}\'");
+
+            if (email == null)
+            {
+                result = Redirect("/auth");
+            }
+            else
+            {
+                userRepository.ConfirmEmail(token, email);
+                userRepository.DeleteToken($"\'{token}\'");
+
+                result = Redirect("/auth");
+                //TODO: Auth
+            }
+
+            return result;
+        }
     }
 }
