2622b81272d447f2d39b334fe8b7ed08797dac32
Mew_boy33 <pugach@dlit.dp.ua>
2019-08-11 15:02:33 +0300

    #129 Registration moved to new client with new component principle refactoring

--- Cooper/ClientApp/src.v2/app/components/registration-form/registration-form.component.ts
+++ Cooper/ClientApp/src.v2/app/components/registration-form/registration-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {SocialNetwork} from '@enums';
 
 @Component({
   selector: 'coop-registration-form',
@@ -7,9 +9,103 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  registrationForm: FormGroup;
+  emptyFieldWarning = 'This field is empty';
+
+  @Output() signUp = new EventEmitter<any>();
+  @Output() socialSignIn = new EventEmitter<SocialNetwork>();
+
+  constructor(private formBuilder: FormBuilder) {
+    this.registrationForm = this.formBuilder.group({
+      Nickname: ['', Validators.required],
+      Email: ['', [Validators.email, Validators.required]],
+
+      Passwords: this.formBuilder.group({
+        Password: ['', [Validators.required, Validators.minLength(4)]],
+        ConfirmPassword: ['', Validators.required]
+      }, { validator: this.comparePasswords })
+    });
+   }
 
   ngOnInit() {
   }
 
+  private onSignUpClicked(): void {
+
+    const body = {
+      Name: this.registrationForm.value.Nickname,
+      Nickname: this.registrationForm.value.Nickname,
+      Email: this.registrationForm.value.Email,
+      Password: this.registrationForm.value.Passwords.Password
+    };
+
+    this.signUp.emit(body);
+  }
+
+  private onSocialSignInClicked(network: SocialNetwork): void {
+    this.socialSignIn.emit(network);
+  }
+
+  private getEmailErrorMessage() {
+
+    const fieldName = 'Email';
+
+    if (this.registrationForm.controls[fieldName].touched) {
+
+      if (this.registrationForm.controls[fieldName].hasError('email')) {
+        return 'Invalid email adress';
+      } else if (this.registrationForm.controls[fieldName].hasError('required')) {
+        return this.emptyFieldWarning;
+      }
+    }
+  }
+
+  private getNicknameErrorMessage() {
+    const fieldName = 'Nickname';
+
+    if (this.registrationForm.controls[fieldName].touched && this.registrationForm.controls[fieldName].hasError('required')) {
+      return this.emptyFieldWarning;
+    }
+  }
+
+  private getPasswordErrorMessage() {
+    const fieldName = 'Passwords.Password';
+
+    if (this.registrationForm.get(fieldName).touched) {
+
+      if (this.registrationForm.get(fieldName).hasError('minlength')) {
+      return 'Minimum 4 characters required!';
+    } else if (this.registrationForm.get(fieldName).hasError('required')) {
+      return this.emptyFieldWarning;
+      }
+    }
+  }
+
+  private getConfirmPasswordErrorMessage() {
+    const fieldName = 'Passwords.ConfirmPassword';
+
+    if (this.registrationForm.get(fieldName).touched) {
+
+      if (this.registrationForm.get(fieldName).hasError('required')) {
+      return this.emptyFieldWarning;
+    } else if (this.registrationForm.get(fieldName).hasError('passwordMismatch')) {
+      return 'Passwords are not the same.';
+    }
+  }
+  }
+
+  private comparePasswords(registrationForm: FormGroup) {
+
+    const confirmPswrdCtrl = registrationForm.get('ConfirmPassword');
+
+    if (confirmPswrdCtrl.errors == null || 'passwordMismatch' in confirmPswrdCtrl.errors) {
+
+      if (registrationForm.get('Password').value !== confirmPswrdCtrl.value) {
+        confirmPswrdCtrl.setErrors({ passwordMismatch: true });
+      } else {
+        confirmPswrdCtrl.setErrors(null);
+      }
+
+    }
+    }
 }
--- Cooper/ClientApp/src.v2/app/layouts/registration/registration.layout.ts
+++ Cooper/ClientApp/src.v2/app/layouts/registration/registration.layout.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Router } from '@angular/router';
+import { UserService, RegistrationService, AuthentificationService } from '@services';
+import {SocialNetwork} from '@enums';
 
 @Component({
   selector: 'coop-registration-layout',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationLayoutComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
 
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private service: RegistrationService,
+              private authService: AuthentificationService, private router: Router) {
+    if (this.authService.isAuthentificated()) {
+      this.router.navigate(['/platform/home']);
+    }
+  }
+
+  private registerUser(body: any) {
+    this.service.register(body);
+  }
+
+  private socialSignIn(platform: SocialNetwork) {
+    this.authService.socialSignIn(platform);
+  }
 }
