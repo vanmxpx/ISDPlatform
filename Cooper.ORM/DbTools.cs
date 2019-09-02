dc779d70f7b605bd013591a819e8bf0063985abd
Mew_boy33 <pugach@dlit.dp.ua>
2019-09-02 19:51:10 +0300

    Added Delete method in DbTools

--- Cooper.ORM/DbTools.cs
+++ Cooper.ORM/DbTools.cs
@@ -73,6 +73,13 @@ namespace Cooper.ORM
             return String.Format("UPDATE {0} SET {1}{2}", table, string.Join(",", entity.attributeValue.Select(x => x.Key + "=" + x.Value).ToArray()), where);
         }
 
+        public static string CreateDeleteQuery(string table, WhereRequest[] whereRequests = null)
+        {
+            string where = CreateWhereRequest(whereRequests);
+
+            return String.Format("DELETE FROM {0} {1}", table, where);
+        }
+
         private static string CreateWhereRequest(WhereRequest[] whereRequests = null)
         {
             string where = "";
