16eb0509195ee50880bf05f81e4f11e4135c9d1a
Mew_boy33 <pugach@dlit.dp.ua>
2019-09-06 18:04:14 +0300

    #85 Deleted unnecessary models

--- Cooper.Models/CommonMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cooper.Models
-{
-    public class CommonMessage
-    {
-        public string Content { get; set; }
-        public int CreateDate { get; set; }
-        public User Author { get; set; }
-    }
-}
